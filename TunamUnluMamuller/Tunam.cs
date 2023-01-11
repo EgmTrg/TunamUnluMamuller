f1eda9abc72f80cb277a387c5c62d3a613fd3c36
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2023-01-12 00:27:02 +0300

    publish: Now checks is chrome driver exist?

--- TunamUnluMamuller/Tunam.cs
+++ TunamUnluMamuller/Tunam.cs
@@ -1,25 +1,28 @@
 ﻿using TunamUnluMamuller.Branches;
 using System.Windows.Forms;
 using System;
-using System.Runtime.ConstrainedExecution;
-using System.Reflection;
+using System.Diagnostics;
 
-namespace TunamUnluMamuller
-{
-    public partial class Tunam : Form
-    {
+namespace TunamUnluMamuller {
+    public partial class Tunam : Form {
         private Web.Informations webInfo;
         private Web web;
 
-        public Tunam()
-        {
+        public Tunam() {
             InitializeComponent();
         }
 
-        private void run_button_Click(object sender, EventArgs e)
-        {
-            if (!dilimBorek_radioButton.Checked && !musluoglu_radioButton.Checked)
-            {
+        private void Tunam_Load(object sender, EventArgs e) {
+            IsExistChromeDriver();
+        }
+
+        private void Tunam_FormClosing(object sender, FormClosingEventArgs e) {
+            web?.Quit();
+        }
+
+        #region ButtonEvents
+        private void run_button_Click(object sender, EventArgs e) {
+            if (!dilimBorek_radioButton.Checked && !musluoglu_radioButton.Checked) {
                 MessageBox.Show("Dilim Börek ya da Musluoğlu markalarından birini seçiniz!", "Marka Seç", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
@@ -46,22 +49,44 @@ namespace TunamUnluMamuller
                 MessageBox.Show("Veri çekme işlemi bir hata sonucu tamamlanamadi.", "Verileri Çek!", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        private void clear_button_Click(object sender, EventArgs e)
-        {
+        private void clear_button_Click(object sender, EventArgs e) {
             dataGridView1.Rows.Clear();
             dataGridView1.Columns.Clear();
             richTextBox1.Clear();
         }
 
-        private void exportToExcel_button_Click(object sender, EventArgs e)
-        {
+        private void exportToExcel_button_Click(object sender, EventArgs e) {
+            MessageBox.Show(
+                "Excel'e kopyalama işlemi, Microsoft Office Excel uygulamanız lisanslı değil ise hata verme ihtimali çok yüksektir.!",
+                "MS Office Excel",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
             ExcelOperations.ExportWithExcel(dataGridView1);
             MessageBox.Show("Excel aktarımı başarıyla tamamlanmıştır.", "Dışa Aktar!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+        #endregion
 
-        private void Tunam_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            web?.Quit();
+        #region Methods
+        private void IsExistChromeDriver() {
+            if (System.IO.File.Exists(@"C:\Windows\chromedriver.exe"))
+                return;
+
+            DialogResult dialog = MessageBox.Show(
+                "ChromeDriver.exe dosyası bulunamadı. Uygulamayı kullanabilmek için indirmeniz gerekmekte!",
+                "ChromDriver Error",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+            if (dialog == DialogResult.OK) {
+                string chromeDriver_downloadURL = ("https://chromedriver.chromium.org/downloads");
+                Process.Start(chromeDriver_downloadURL);
+            }
+            Application.Exit();
+        }
+        #endregion
+
+        private void tarayiciyiGösterToolStripMenuItem_Click(object sender, EventArgs e) {
+            tarayiciyiGösterToolStripMenuItem.Checked = !tarayiciyiGösterToolStripMenuItem.Checked;
+            MessageBox.Show(tarayiciyiGösterToolStripMenuItem.Checked.ToString());
         }
     }
 }
