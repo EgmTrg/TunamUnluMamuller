2b74a18fafdc8b072038a2e9b74ade45d8c7a4e5
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2023-01-12 22:52:10 +0300

    +added: Settings menu.

--- TunamUnluMamuller/Properties/Resources.Designer.cs
+++ TunamUnluMamuller/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace TunamUnluMamuller.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap settings_png {
+            get {
+                object obj = ResourceManager.GetObject("settings_png", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
